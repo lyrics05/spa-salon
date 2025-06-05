720a1d6bc19e06493a6776c0d531ee98470b2ea7
lyrics05 <dca49@hotmail.com>
2025-06-05 14:25:29 -0600

    cambios en gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,54 +7,58 @@ const autoprefixer = require('autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const cssnano = require('cssnano');
 
-// Imagenes
+// Imágenes
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 
-function css( done ) {
+// Detectar si estamos en Netlify
+const isNetlify = process.env.NETLIFY === 'true';
+
+function css(done) {
     src('src/scss/app.scss')
-        .pipe( sourcemaps.init() )
-        .pipe( sass() )
-        .pipe( postcss([ autoprefixer(), cssnano() ]) )
-        .pipe( sourcemaps.write('.'))
-        .pipe( dest('build/css') )
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/css'));
 
     done();
 }
 
 function imagenes() {
     return src('src/img/**/*')
-        .pipe( imagemin({ optimizationLevel: 3 }) )
-        .pipe( dest('build/img') )
+        .pipe(imagemin({ optimizationLevel: 3 }))
+        .pipe(dest('build/img'));
 }
 
 function versionWebp() {
-    const opciones = {
-        quality: 50
-    }
+    const opciones = { quality: 50 };
     return src('src/img/**/*.{png,jpg}')
-        .pipe( webp( opciones ) )
-        .pipe( dest('build/img') )
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'));
 }
+
 function versionAvif() {
-    const opciones = {
-        quality: 50
+    if (isNetlify) {
+        console.log('⚠️  Saltando generación AVIF en Netlify');
+        return Promise.resolve(); // No hace nada
     }
+
+    const opciones = { quality: 50 };
     return src('src/img/**/*.{png,jpg}')
-        .pipe( avif( opciones ) )
-        .pipe( dest('build/img'))
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'));
 }
 
 function dev() {
-    watch( 'src/scss/**/*.scss', css );
-    watch( 'src/img/**/*', imagenes );
+    watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', imagenes);
 }
-
 
 exports.css = css;
 exports.dev = dev;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.default = series( imagenes, versionWebp, versionAvif, css, dev  );
+exports.default = series(imagenes, versionWebp, versionAvif, css, dev);
